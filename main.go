9661ea3029d0826e4bc47233c92fbe040a19bf39
brettlazarine <jblazarine@gmail.com>
2025-01-08 12:17:15 -0600

    CH2 L1 -> apiConfig to track hits, metrics and reset handlers

--- main.go
+++ main.go
@@ -1,23 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+type apiConfig struct {
+	fileserverHits atomic.Int32
 }
 
 func main() {
-	const port = "8080"
 	const filePathRoot = "."
+	const port = "8080"
+	cfg := &apiConfig{}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	fileServer := http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(fileServer))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", cfg.handlerMetrics)
+	mux.HandleFunc("/reset", cfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -26,4 +30,17 @@ func main() {
 	log.Printf("Serving files from %v on port: %v", filePathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 	defer srv.Close()
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("middlewareMetricsInc")
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
 }
