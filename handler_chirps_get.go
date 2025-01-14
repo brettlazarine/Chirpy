2d81a4f3afe85df649a4e61db4abd2e669727f71
brettlazarine <jblazarine@gmail.com>
2025-01-14 11:26:06 -0600

    CH9 L1 -> get chirps by author (with boot.dev code commented for reference)

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -3,14 +3,31 @@ package main
 import (
 	"net/http"
 
+	"github.com/brettlazarine/Chirpy/internal/database"
 	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerGetAllChirps(w http.ResponseWriter, r *http.Request) {
-	dbChirps, err := cfg.db.GetAllChirps(r.Context())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "could not get chirps", err)
-		return
+	author_id := r.URL.Query().Get("author_id")
+	var dbChirps []database.Chirp
+	var err error
+	if author_id != "" {
+		authorId, err := uuid.Parse(author_id)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "error parsing author id", err)
+			return
+		}
+		dbChirps, err = cfg.db.GetChirpsByAuthor(r.Context(), authorId)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "could not get chirps", err)
+			return
+		}
+	} else {
+		dbChirps, err = cfg.db.GetAllChirps(r.Context())
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "could not get chirps", err)
+			return
+		}
 	}
 
 	chirps := make([]Chirp, len(dbChirps))
@@ -52,3 +69,39 @@ func (cfg *apiConfig) handlerGetChirp(w http.ResponseWriter, r *http.Request) {
 
 	respondWithJSON(w, http.StatusOK, chirp)
 }
+
+// *** Boot.dev Implementation
+// func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
+// 	dbChirps, err := cfg.db.GetChirps(r.Context())
+// 	if err != nil {
+// 		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
+// 		return
+// 	}
+
+// 	authorID := uuid.Nil
+// 	authorIDString := r.URL.Query().Get("author_id")
+// 	if authorIDString != "" {
+// 		authorID, err = uuid.Parse(authorIDString)
+// 		if err != nil {
+// 			respondWithError(w, http.StatusBadRequest, "Invalid author ID", err)
+// 			return
+// 		}
+// 	}
+
+// 	chirps := []Chirp{}
+// 	for _, dbChirp := range dbChirps {
+// 		if authorID != uuid.Nil && dbChirp.UserID != authorID {
+// 			continue
+// 		}
+
+// 		chirps = append(chirps, Chirp{
+// 			ID:        dbChirp.ID,
+// 			CreatedAt: dbChirp.CreatedAt,
+// 			UpdatedAt: dbChirp.UpdatedAt,
+// 			UserID:    dbChirp.UserID,
+// 			Body:      dbChirp.Body,
+// 		})
+// 	}
+
+// 	respondWithJSON(w, http.StatusOK, chirps)
+// }
